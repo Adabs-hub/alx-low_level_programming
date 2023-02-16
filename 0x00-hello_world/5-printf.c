66611902e4c7758bf4afd36f23bc699fc6180f74
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-16 12:07:41 -0800

    test 5: hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Desription: 'writing by first betty documentations and styling
+ *
+ * Return: zero upon success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
