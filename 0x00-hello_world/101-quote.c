ccdf39c005f47bbe4bdf4bd8f6dfb1c769ea70e7
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-16 13:24:09 -0800

    optional test 8: std err

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Desription: 'description'
+ *
+ * Return: zero upon success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (-1);
+}
