6a412e2a0a51ab66918635b8b09b14e16366c89b
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-16 10:18:14 -0800

    correction hello puts: test 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(int)
+int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
