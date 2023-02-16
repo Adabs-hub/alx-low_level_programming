feae12a3520a840c14f08172c49786246abf2646
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-16 11:36:17 -0800

    betty documentiona style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 /**
- * my first program in betty styling
- **/
+ * main()
+ * my first program in betty styling programm
+ * The entry funtion: main();
+ * takes no argument
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");	
+	/**
+	 * the return statement
+	 * returns 0 upon successfully comletion
+	 */
+	return(0);
 }
