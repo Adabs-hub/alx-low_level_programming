75293199c7ba6f2038c33faab008df6c4a924bb4
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-17 13:24:15 -0800

    7-print_tebahpla.c test 7: coplete

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'positive anything is better than negative nothing'
+  *
+  * Return: zero on success
+  */
+int main(void)
+{
+	int c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
