d6df95359c2fe726d4b9d1df647198869b633845
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-17 12:12:00 -0800

    3-print_alphabets.c test 3 completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'positive anything is better than negative nothing'
+  *
+  * Return: zero on success
+  */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
