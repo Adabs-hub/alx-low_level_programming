975073a6bb6f774a30121186cc50d8b82cb0958e
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-17 13:04:37 -0800

    5-print_numbers.c test 5: complete

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'positive anything is better than negative nothing'
+  *
+  * Return: zero on success
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
