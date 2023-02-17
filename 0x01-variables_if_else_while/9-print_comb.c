be31d9b531772ebcdc6f17bc9e7e02f4fcebbf6b
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-17 14:34:57 -0800

    9-print_comb.c test 9: complete

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'positive anything is better than negative nothing'
+  *
+  * Return: zero on success
+  */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (i != '0')
+				putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+		j = '0';
+	}
+	return (0);
+}
