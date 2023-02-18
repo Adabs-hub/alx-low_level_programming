6c62747017939e7b001f75abdcb396e6ba7f2d24
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-17 17:49:57 -0800

    correction initial declarations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,12 @@
   */
 int main(void)
 {
-	for (int i = 0; i < 100; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
 	{
-		for (int j = 0; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
 			{
