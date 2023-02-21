68db3ae9046a27fc94074579497a267078699b52
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-21 15:27:12 -0800

    test corrections

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,10 @@
  * Author: Adabogo Emmanuel
  */
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _putchar(char c);
 int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
 
 #endif
