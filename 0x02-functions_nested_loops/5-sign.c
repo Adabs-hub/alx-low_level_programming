260445ecb820450a6c883518559254aba7d5a0d2
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-21 17:42:39 -0800

    sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - main Entry
+ *
+ * @c: is an integer number
+ *
+ * Description: indicates the sign of a number and zero if zero
+ *
+ * Return: +1 for positive, 0 for zero, -1 for negative
+ */
+
+int _isalpha(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n <0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else{
+		_putchar('0');
+		return (0);
+	}
+}
