05b77465697f0639eac8df541e532a3d5189b34c
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-21 19:58:06 -0800

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,39 +10,22 @@
 
 void jack_bauer(void)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int m = 0;
+	int i, j;
 
-	while (i <= 2)
+	i = 0;
+	while (i < 24)
 	{
-		while (j < 10)
-		{
-			while (k < 10)
-			{
-				while (m < 10)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-					if (i == 2 && j == 3 && k == 5 && m == 9)
-					{
-						break;
-					}
-					m++;
-				}
-				k++;
-				m = 0;
-			}
-			j++;
-			k = 0;
-		}
-		i++;
 		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+	i++;
 	}
-
 }
