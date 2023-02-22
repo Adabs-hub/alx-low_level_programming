4f2650661369c68cf1eee546ef668ed1c26a5637
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-21 17:47:07 -0800

    correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@
  * Return: +1 for positive, 0 for zero, -1 for negative
  */
 
-int _isalpha(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n <0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
