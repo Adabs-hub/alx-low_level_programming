14dab9545fe272c7263e3ed0444fd9c18995f4fe
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-21 18:26:41 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - main Entry
+ *
+ * @n: is an integer number
+ *
+ * Description: takes a number an returns an absolute value
+ *
+ * Return: return the absolute value
+ */
+
+int _abs(int n)
+{
+	int l_digit;
+
+	l_digit = n % 10;
+	return (l_digit);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int print_sign(int n);
+int print_last_digit(int n);
 #endif
