42e931a3dead475af8bb4fc606535f98aa754950
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 11:55:38 -0800

    day of the year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *larget_number - returns the largest of 3 numbers
+ *largest_number - returns the largest of 3 numbers
  *@a: first integer
  *@b: second integer
  *@c: third integer
