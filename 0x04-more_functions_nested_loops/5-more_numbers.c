7cb297cdc3d91a7815c4f92d5d49b5da0b9ea11d
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 18:23:15 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * most_numbers - print 0 to 9
+ *
+ * Description: print 1 to 14
+ *
+ * Return: nothing
+ */
+
+void most_numbers(void)
+{
+	char str[] = "01234567891011121314";
+	int i, j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0;  i <= 20; i++)
+			_putchar(str[i]);
+	_putchar('\n');
+	j++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
