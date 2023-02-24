0c194430be2d8534ea09ce055bc92ccf6ac844ee
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 21:38:07 -0800

    triangle print

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_triangle(int size);
 void Print(int);
 void print_line(int n);
 void print_diagonal(int n);
