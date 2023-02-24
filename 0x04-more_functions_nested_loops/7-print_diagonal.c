5b2de77a08ec9d166b67403118475e77fa9c2896
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 21:03:40 -0800

    correcitons

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,15 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
-	for (i = 0;  i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-			_putchar(' ');
-		_putchar('\\');
+	
+	if (n <= 0)
 		_putchar('\n');
-	}
-	_putchar('\n');
+	else
+		for (i = 0;  i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 }
