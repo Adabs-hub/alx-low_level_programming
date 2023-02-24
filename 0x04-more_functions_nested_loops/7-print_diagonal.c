5ad1b8fe25d7728a727a992669cdde890057da13
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 21:09:37 -0800

    correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	
+
 	if (n <= 0)
 		_putchar('\n');
 	else
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0;  i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
-	}
-	_putchar('\n');
+	else
+		for (i = 0;  i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 }
