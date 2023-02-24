3726e663ed9f317b0384b65e3add075593f12467
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-23 21:30:17 -0800

    correciton l" "

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,10 +18,10 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
+		else if (i % 5 == 0 && i == 100)
+			printf("Buzz");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i == 100)
-			printf("%d", i);
 		else
 			printf("%d ", i);
 	}
