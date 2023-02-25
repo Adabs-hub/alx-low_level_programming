48bb32c09b0dcacbebc13e5d52e0a77187835a6f
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-25 09:51:48 -0800

    100-times_table.c push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,37 +33,36 @@ void table(int *i, int *j, int n)
 	if (*j == 0)
 	{
 		Print(0);
-		if (*j != *i)
+		if (*j == n)
 			_putchar(',');
 	}
 	else
-		if (*j != n)
+	{
+		if (*j * *i < 10 && *j * *i >= 0)
 		{
-			if (*j * *i < 10 && *j * *i >= 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				Print(*j * *i);
-				if (*j != *i)
-					_putchar(',');
-			}
-			else if (*j * *i < 100 && *j * *i > 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				Print(*j * *i);
-				if (*j != *i)
-					_putchar(',');
-			}
-			else
-			{
-				_putchar(' ');
-				Print(*j * *i);
-				if (*j != *i)
-					_putchar(',');
-			}
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			Print(*j * *i);
+			if (*j == n)
+				_putchar(',');
 		}
+		else if (*j * *i < 100 && *j * *i > 9)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			Print(*j * *i);
+			if (*j == n)
+				_putchar(',');
+		}
+		else
+		{
+			_putchar(' ');
+			Print(*j * *i);
+			if (*j == n)
+				_putchar(',');
+		}
+	}
 	++*j;
 }
 
