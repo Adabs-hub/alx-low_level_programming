6773c24b529554de17989224bf5a96acec0cc85b
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-27 12:14:30 -0800

    I can only go way: test 4_ print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse strings
+ *
+ * @s: source string arg
+ *
+ * Return: nothing
+ */
+
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	while (i => 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
