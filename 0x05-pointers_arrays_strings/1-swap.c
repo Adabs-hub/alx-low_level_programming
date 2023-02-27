445b2a2af3b73c23bee4d688b1e18838037714d1
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-27 10:43:20 -0800

    test 1: Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * swap_int - swap intergers
+ *
+ * @a: first int arg
+ *
+ * @b: second int arg
+ *
+ * Returns: nothing (void)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
