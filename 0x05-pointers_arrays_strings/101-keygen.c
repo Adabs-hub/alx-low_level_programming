4fc20ef557973f281112039431cefe5a017756b8
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-27 20:35:13 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	char passwd[84];
 	int i = 0, sum = 0;
-	int first_diff, second_diff;
+	int second_diff;
 
-	srand(tim(0));
+	srand(time(0));
 	while (sum < 2772)
 	{
 		passwd[i] = 33 + rand() % 94;
@@ -27,7 +27,7 @@ int main(void)
 		second_diff = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			for (i = 0; passwd[i]; i++)
-				password[i] -= second_diff;
+				passwd[i] -= second_diff;
 	}
 	printf("%s", passwd);
 	return (0);
