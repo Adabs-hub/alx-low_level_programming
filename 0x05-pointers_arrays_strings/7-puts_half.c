005f31b0fc746aa995bdcfebf8fe24c1e0054fa2
Adabs-hub <emmanueladabogo@gmail.com>
2023-02-27 17:30:58 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,8 +19,8 @@ void puts_half(char *str)
 		for (j = (size) / 2; j < size; j++)
 			_putchar(str[j]);
 	else
-		for (j = (size - 1) / 2; j < size; j++)
-			_putchar(str[j - 1]);
+		for (j = ((size - 1) / 2) + 1; j < size; j++)
+			_putchar(str[j]);
 	_putchar('\n');
 
 }
