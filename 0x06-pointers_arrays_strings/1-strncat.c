2262c3f99fc8b035a80d9e61fcc1c5a93187341c
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-01 15:41:00 -0800

    strncat function test 1:

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - find string len
+ *
+ * @s: input string
+ *
+ * Return: string size
+ */
+
+int _strlen(char *s)
+{
+	char c = s[0];
+	int size = 0;
+
+	while (c != '\0')
+	{
+		size++;
+		c = s[size];
+	}
+	return (size);
+}
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: 1st arg - destination string
+ *
+ * @src: 2nd arg - source string
+ *
+ * @n: number of char to concatenate to string
+ *
+ * Return: destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dest_sz = 0, src_sz = 0;
+
+	dest_sz = _strlen(dest);
+	src_sz = _strlen(src);
+	while (i < n && n <= src_sz)
+	{
+		dest[dest_sz] = src[i];
+		i++;
+		dest_sz++;
+	}
+	return (dest);
+}
