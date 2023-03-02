232e5810e29ed121873173ad60c97b963a2396b7
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-01 17:49:58 -0800

    string compare

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
 
 #endif /*MAIN_H*/
