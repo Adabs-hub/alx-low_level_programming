63e1c1e3284a73afd7093a4abbfb411387789f8c
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-01 17:27:59 -0800

    string copy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _atoi(char *s);
 
 #endif /*MAIN_H*/
