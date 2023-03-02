4a5488ceed390f3962e36a7ff4ecf38c0c5a1ae2
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-01 18:37:42 -0800

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -33,7 +33,7 @@ int _strlen(char *s)
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, result, s1_len = 0, s2_len = 0;
+	int i = 0, result = 0, len, s1_len = 0, s2_len = 0;
 
 	s1_len = _strlen(s1);
 	s2_len = _strlen(s2);
@@ -43,7 +43,7 @@ int _strcmp(char *s1, char *s2)
 		len = s2_len;
 	while (i <= len)
 	{
-		result = s1[i] - s2[1];
+		result = s1[i] - s2[i];
 		if (result != 0)
 			return (result);
 		i++;
