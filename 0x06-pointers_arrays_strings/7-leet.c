91444243a2e3a61aa237941a6e3cd81a34e2975b
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-02 16:32:07 -0800

    encode string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * leet - selective charaters to int
+ *
+ * @str: source string
+ *
+ * Return: modified char
+ */
+
+char *leet(char *str) 
+{
+	char key[] = "4433007711";
+	char code[] = "aAeEoOtTlL";
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+	{
+		while (j < 10)
+		{
+			if ( str[i] == code[j])
+				str[i] = key[j];
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
+char *leet(char *str);
 int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
 void reverse_array(int *a, int n);
