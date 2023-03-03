1465c782c28dd413e7e3991d92452bbea338604b
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-02 17:01:34 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - selective charaters to int
+ *
+ * @str: source string
+ *
+ * Return: modified char
+ */
+
+char *rot13(char *str)
+{
+	char key[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char code[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+	{
+		while (j < 10)
+		{
+			if (str[i] == code[j])
+				str[i] = key[j];
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *rot13(char *str);
 int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
 void reverse_array(int *a, int n);
