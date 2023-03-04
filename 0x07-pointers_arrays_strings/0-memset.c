c34464477e4da5d4daeeb8e5caa605ca25533d26
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-04 15:19:18 -0800

    task 0: memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with constant byte
+ *
+ * @s: string
+ *
+ * @b: char to initial with
+ *
+ * @n: n byte pointed by memory
+ *
+ * Return: modified string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n, i++)
+		*(s + i) = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen(char *s);
+char *_memset(char *sd, char b, unsigned int n);
+
+#endif /*MAIN_H*/
