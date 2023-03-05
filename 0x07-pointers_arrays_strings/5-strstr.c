ed6959044265b445452557f5f1faea438a4a5396
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-05 07:56:11 -0800

    strstr function. find word instring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - search string for any set of byte
+ *
+ * @haystack: source string
+ *
+ * @needle: prefix substring
+ *
+ * Return: pointer to the byte in haystack that
+ * matches one of the bytes in needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+	char *first_byte = 0;
+
+	while (haystack[i] != '\0')
+	{
+		while (needle[j] == haystack[i + j])
+		{
+			if (first_byte == 0)
+				first_byte = (haystack + i);
+			if (needle[j + 1] == '\0')
+				return (first_byte);
+			j++;
+		}
+		first_byte = 0;
+		i++;
+		j = 0;
+
+	}
+	return (first_byte);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 
 #endif /*MAIN_H*/
