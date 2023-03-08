dadee438f329bd0a85208d0174b516756832d5fc
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-08 14:05:04 -0800

    recursion print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string
+ *
+ * @s: first argument. source string
+ *
+ * Return: returns nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
