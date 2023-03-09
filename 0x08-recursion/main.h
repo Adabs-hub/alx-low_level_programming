77541c0f074d5916ff2e335d8e160668a404962d
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-08 17:12:52 -0800

    func label. sqrt -> _sqrt

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int sqrt(int n, int i);
+int _sqrt(int n, int i);
 int is_prime_number(int n);
 int divisors(int n, int m);
 void _puts_recursion(char *s);
