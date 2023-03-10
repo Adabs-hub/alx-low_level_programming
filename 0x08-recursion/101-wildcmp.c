6f301e5ac38b6a7dfc8bd51ccfb0adc10cd74d5f
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-09 20:15:02 -0800

    wild card compare string

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - print string len
+ *
+ * @s: first argument. source string
+ *
+ * Return: string length
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
+
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s2, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	return (0);
+}
