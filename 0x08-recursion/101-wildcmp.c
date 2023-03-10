099f23d3bc0e5014b13588c45c12383bdbf04fd8
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-09 20:19:37 -0800

    wildcmp func. correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s2, s2 + 1) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
 }
