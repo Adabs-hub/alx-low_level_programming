0232443f4e58ed5615693cb52cd5e41fac9b2757
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-10 18:26:45 -0800

    prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * @argc: arg count
+ *
+ * @argv: arguments string vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
