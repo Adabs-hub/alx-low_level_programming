b70d1769e17e49f87e17e75c4fa0b1944547c926
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-10 19:07:32 -0800

    infinite add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry
+ *
+ * @argc: arg count
+ *
+ * @argv: arguments string vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (-1);
+			}
+			sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+
+	return (0);
+}
