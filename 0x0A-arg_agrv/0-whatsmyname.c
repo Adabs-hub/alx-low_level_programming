1cd8efe756d781260f852211c4f69e73d1d340f2
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-10 17:54:52 -0800

    program that prints its name

---
+++ 0x0A-arg_agrv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * @argc: arg count
+ *
+ * @argv: arguments string vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
