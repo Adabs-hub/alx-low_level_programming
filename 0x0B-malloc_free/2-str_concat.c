417eca8afba21f654d08aa0e66c1e2c5c3c44acf
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-14 11:56:45 -0700

    malloc: concatenate string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - to fine string len
+ *
+ * @str: string
+ *
+ * Return: length on success
+ */
+
+int _strlen(char *str)
+{
+	int i = 1;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * str_concat - concatenate two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * Return: NULL on failure and pointer to location in memory on success
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *buffer = 0;
+	int i = 0, len1 = 0, len2 = 0;
+
+	if (s1 == 0)
+		len1 = 0;
+	else if (s2 == 0)
+		len2 = 0;
+	else
+	{
+		len1 = _strlen(s1);
+		len2 = _strlen(s2);
+	}
+	buffer = (char *)malloc(sizeof(char) *	(len1 + len2));
+	if (buffer == 0)
+		return (0);
+	while (i < len1 + len2)
+	{
+		if (i < len1)
+			buffer[i] = s1[i];
+		else
+			buffer[i] = s2[i - len1];
+		i++;
+	}
+	return (buffer);
+}
