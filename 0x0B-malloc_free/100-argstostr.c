7d5d53829a2755fc6e9cacff777d0bcc3ecbab93
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-14 21:27:21 -0700

    test 4

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - to fine string len
+ *
+ * @str: string
+ *
+ * Return: length on success
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * argstostr - concatenate two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * Return: NULL on failure and pointer to location in memory on success
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *buffer = 0;
+	int i = 0, len1 = 0, len2 = 0;
+
+	if (ac == 0 && av == 0)
+		i++;
+	else if (s1 == 0)
+	{
+		len1 = 0;
+		len2 = _strlen(s2);
+	}
+	else if (s2 == 0)
+	{
+		len2 = 0;
+		len1 = _strlen(s1);
+	}
+	else
+	{
+		len1 = _strlen(s1);
+		len2 = _strlen(s2);
+	}
+	buffer = (char *)malloc(sizeof(char) *	(len1 + len2) + 1);
+	if (buffer == 0)
+		return (0);
+	if (i > 0)
+		return (buffer);
+	while (i < len1 + len2)
+	{
+		if (i < len1)
+			buffer[i] = s1[i];
+		else
+			buffer[i] = s2[i - len1];
+		i++;
+	}
+	return (buffer);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		buffer[i] = malloc(sizeof(int) * width);
 		if (buffer[i] == NULL)
 		{
-			for(i = 0; i < height; i++)
+			for (i = 0; i < height; i++)
 				free(buffer[i]);
 			free(buffer);
 			return (0);
