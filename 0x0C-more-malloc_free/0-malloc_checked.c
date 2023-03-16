3d65c3d13c9940ea46938d96860de433f65b0dbb
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-16 12:22:24 -0700

    malloc_C. allocate memory using malloc

---
+++ 0x0C-more-malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b: size memory to allocate
+ *
+ * Return: pointer to allocated space or exite 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void * buffer = malloc(b);
+	if (!buffer)
+		noreturn void exit(98);
+	return (buffer);
+}
---
+++ 0x0C-more-malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen(char *str);
+char *malloc_checked(unsigned int b);
+
+#endif /*MAIN_H*/
