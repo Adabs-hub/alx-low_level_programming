65941515935232af67e5c350649724329e03eb93
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-16 16:25:58 -0700

    test 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - to fine string len
+ *
+ * @str: string
+ *
+ * Return: length on success
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenate two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * @n: number of n byte
+ *
+ * Return: NULL on failure and pointer to location in memory on success
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *buffer = 0;
+	int i = 0, len1 = 0, len2 = 0;
+
+	if (s1 == 0 && s2 == 0)
+		i++;
+	else if (s1 == 0)
+	{
+		len1 = 0;
+		len2 = _strlen(s2);
+	}
+	else if (s2 == 0)
+	{
+		len2 = 0;
+		len1 = _strlen(s1);
+	}
+	else
+	{
+		len1 = _strlen(s1);
+		len2 = _strlen(s2);
+	}
+	if (n > len2)
+		n = len2;
+
+	buffer = (char *)malloc(sizeof(char) *	(len1 + n) + 1);
+	if (buffer == 0)
+		return (0);
+	if (i > 0)
+		return (buffer);
+	while (i < len1 + n)
+	{
+		if (i < len1)
+			buffer[i] = s1[i];
+		else
+			buffer[i] = s2[i - len1];
+		i++;
+	}
+	return (buffer);
+}
