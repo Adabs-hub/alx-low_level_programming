6f7976e8a02bf834427290627c4c4cff6eadb364
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-16 19:14:42 -0700

    test 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,16 +35,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *buffer = 0;
 	unsigned int i = 0, len1 = 0, len2 = 0;
 
-	if (s1 == 0 && s2 == 0)
-		i++;
 	else if (s1 == 0)
 	{
+		s1 = "";
 		len1 = 0;
 		len2 = _strlen(s2);
 	}
 	else if (s2 == 0)
 	{
 		len2 = 0;
+		s2 = "";
 		len1 = _strlen(s1);
 	}
 	else
@@ -55,11 +55,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > len2)
 		n = len2;
 
-	buffer = malloc(sizeof(char) *	(len1 + n + 1));
+	buffer = malloc(sizeof(char) * (len1 + n) + 1);
 	if (buffer == 0)
+	{
 		return (0);
-	if (i > 0)
-		return (buffer);
+	}
 	while (i < len1 + n)
 	{
 		if (i < len1)
