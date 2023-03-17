ee67415e551b720621e655e55240fce53357c4bc
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-16 17:55:25 -0700

    test 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocate memory and initail zero
+ *
+ * @nmemb: array members
+ *
+ * @size: array size
+ *
+ * Return: pointer to array or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *buffer = 0;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	buffer = malloc(size * nmemb);
+	if (!buffer)
+	{
+		free(buffer);
+		return (0);
+	}
+	for (i = 0; i < nmemb * size; i++)
+		buffer[i] = 0;
+	return (jbuffer);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 int _strlen(char *str);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /*MAIN_H*/
