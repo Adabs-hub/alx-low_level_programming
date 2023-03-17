efe0cdc3e09a2ce39bbccdad395a72572d1efe41
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-16 17:14:22 -0700

    error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ int _strlen(char *str)
 }
 
 /**
- * string_nconcat - concatenate two strings
+ * string_nconcat - concatenate two strings to n byte
  *
  * @s1: first string
  *
@@ -30,7 +30,7 @@ int _strlen(char *str)
  * Return: NULL on failure and pointer to location in memory on success
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconct(char *s1, char *s2, unsigned int n)
 {
 	char *buffer = 0;
 	int i = 0, len1 = 0, len2 = 0;
