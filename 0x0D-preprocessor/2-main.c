15d70fb3d16aac990f50f011779fc2540aecb80a
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-20 14:11:28 -0700

    test 2: compiled file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Desc: print compiled file name
+ * Return: zero on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
