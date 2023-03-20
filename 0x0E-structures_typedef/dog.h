04cce6158fa97efad569fdb8edb503edc1933dce
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-20 15:49:38 -0700

    0.poppy. test )

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - type def for dog
+ * @name: first name
+ * @age: age
+ * @owner: name of owner
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 #endif /*DOG.H*/
