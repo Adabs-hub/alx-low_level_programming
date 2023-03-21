ab67c4c025d04e54994204fd1131fe48d03856ec
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-20 18:53:47 -0700

    new dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,8 +1,17 @@
 #include "dog.h"
 
+/**
+ * free_dog - free dog from memory
+ * @d: struct dog
+ * Return: nothing
+ */
+
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (!d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
