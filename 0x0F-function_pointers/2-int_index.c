659d8ed269ad3bc8159715a01e0d91c38a398911
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-22 16:48:23 -0700

    task 2. search int with compare

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,14 +1,19 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - search integer
+ * @size: size of array
+ * @cmp: compare func
+ * Return: index of searched element in array
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array != null && cmp != null && size > 0)
 		for (i = 0; i < size; i++)
-		{
 			if (cmp(array[i]) == 1)
-					return (i);
-		}
+				return (i);
 	return (-1);
 }
