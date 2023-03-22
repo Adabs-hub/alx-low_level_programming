a2d92364ae56f0e6a7be3f12c71a9d0870fec6d1
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-22 15:08:03 -0700

    task 0: print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print function with a given name
+ *@name: name to print
+ *@f: function to print name
+ *Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
