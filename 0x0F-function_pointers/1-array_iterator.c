821b2ff60128cb6faa370a44088de22b1af71dac
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-22 16:00:25 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	if (array != null && action != null)
-		for ( i = 0; i < size; i++)
+		for( i = 0; i < size; i++)
 			action(array[i]);
 }
