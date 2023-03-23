8167fefd312acdafe21133143fcd0507e2d3d748
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-22 19:14:43 -0700

    task 3. simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 6)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
