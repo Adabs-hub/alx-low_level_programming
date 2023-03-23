d4a53d7efc2e671559d92808268b5f64e3c2c380
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-23 13:41:24 -0700

    task 1. print number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_numbers - print numbers passed to it
+ * @separator: separates numbers with given ch or string
+ * @n: number of args
+ * @...: variadic func args
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (n != 0 && separator != 0)
+		for (i = 0; i < n; i++)
+		{
+			if (i == n - 1)
+				printf("%d\n", va_arg(args, int));
+			else
+				printf("%d%s ", va_arg(args, int), separator);
+		}
+	va_end(args);
+}
