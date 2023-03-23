8a16872d1714aabfff68c96cb391905964cc7b2f
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-23 14:31:32 -0700

    task one

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,13 +16,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator != 0 && strlen(separator) != 0)
+	if (n != 0)
 		for (i = 0; i < n; i++)
 		{
-			if (i == n - 1)
-				printf("%d\n", va_arg(args, int));
+			if (i != n - 1)
+			{
+				printf("%d", va_arg(args, int));
+				if (separator != 0)
+					printf("%s ", separator);
+			}
 			else
-				printf("%d%s ", va_arg(args, int), separator);
+				printf("%d\n", va_arg(args, int));
 		}
 	va_end(args);
 }
