a06d164aa20bf287c851060d48d8a03ba2208133
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-23 16:40:31 -0700

    task 3. pritn all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,20 @@
 #define VARIADIC_FUNCTION_H
 #define null 0
 #include <stdarg.h>
-va_list list;
 
+/**
+ * struct print - A new struct type defining our print
+ * @symbol: Our symbol representing the data type
+ * @print_func: a function pointer that prints
+ */
+
+typedef struct print
+{
+	char *symbol;
+	void (*print_func)(va_list arg);
+} func_printer;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
