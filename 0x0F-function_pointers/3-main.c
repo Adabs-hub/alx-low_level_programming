28ad03f971f0f37c6a9b987f9cfb5ec3b0157ce9
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-22 19:52:09 -0700

    task 3. calculator..

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (atoi(argv[3]) == 0)
+	if (atoi(argv[3]) == 0 || atoi(argv[1]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
