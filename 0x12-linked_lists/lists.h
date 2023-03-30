ff7f86186e9b7a5dc4a943b6514c7ece37eb4891
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-30 16:25:40 -0700

    tast 1:pritn list

---
+++ 0x12-linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/*function definitions*/
+int _putchar(char c);
+size_t print_list(const list_t *h);
+void print_number(size_t n);
+void print_node(char *str, size_t len);
+
+#endif /*list.h*/
