a6210994cb94d8a513701b5b316c8d4d63d469bd
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-30 18:53:06 -0700

    task 2: add list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <string.h>
+
+
+
+/**
+ * _strlen - calculate lenght of strings
+ *
+ * @s: source string arg
+ *
+ * Return: size of string arg char*
+ */
+
+int _strlen(const char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+		size++;
+
+	return (size);
+}
+
+
+list_t *add_node(list_t **h, const char *str)
+{
+	list_t *tmp = 0;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	tmp->str = strdup(str);
+	tmp->len = _strlen(str);
+	if (*h == NULL)
+		tmp->next = NULL;
+	else
+		tmp->next = *h;
+	*h = tmp;
+
+	return (tmp);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,9 +20,11 @@ typedef struct list_s
 
 /*function definitions*/
 	int _putchar(char c);
+	int _strlen(const char *s);
 	size_t print_list(const list_t *h);
 	void print_number(size_t n);
 	void print_node(char *str, size_t len);
 	size_t list_len(const list_t *h);
+	list_t *add_node(list_t **head, const char *str);
 
 #endif /*list.h*/
