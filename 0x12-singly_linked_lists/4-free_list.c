f9957127e1bb947e624b0710f09c18deb4d73b63
Adabs-hub <emmanueladabogo@gmail.com>
2023-03-30 20:15:15 -0700

    task 4: free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free memory
+ * @head: linked list
+ * Return: nothing
+ */
+ 
+void  free_list(list_t *head)
+{
+	list_t *temp = 0;
+	
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
