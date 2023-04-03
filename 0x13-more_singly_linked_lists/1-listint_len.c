688151a0204f4ed024c849a0876c0ea70228659c
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-03 15:19:51 -0700

    task 1: list len update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - list length of a linked list
+ * @h: linked list
+ * Return: number of nodes
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
