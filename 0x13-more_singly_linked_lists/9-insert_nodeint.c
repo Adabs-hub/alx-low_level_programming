72fde7983542747428fe7e2fa89f4b31a2ebb23f
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-04 17:20:14 -0700

    task 9: insert node update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx; i++)
 	{
 		if (next_node->next == NULL)
-			break;
+			return (NULL);
 		previous_node = next_node;
 		next_node = next_node->next;
 	}
