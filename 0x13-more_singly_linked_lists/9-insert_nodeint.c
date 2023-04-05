11a66a572c32f37d086834de3dc6e81ef937ef73
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-04 19:21:18 -0700

    update 11

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/*assign next node to current head*/
 	next_node = *head;
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < (idx - 1); i++)
 	{
 		if (next_node == NULL || next_node->next == NULL)
 			return (NULL);
