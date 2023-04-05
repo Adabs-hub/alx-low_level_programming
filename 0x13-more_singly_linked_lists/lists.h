e03fa30ed1fb9ea8ff0760265e2cc378621b3aa8
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-04 20:16:23 -0700

    task 13: print safe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,6 @@ typedef struct listint_s
 	listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 	int delete_nodeint_at_index(listint_t **head, unsigned int index);
 	listint_t *reverse_listint(listint_t **head);
+	size_t free_listint_safe(listint_t **h);
 
 #endif /*LISTS_H*/
