16880300c68b00319f45c81c487103d54aceb4cd
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-04 19:46:54 -0700

    task 10: delete index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete at index
+ * @head: address of linked list
+ * @index: index of node to delete from
+ * Return:1 on success and -1 if failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *tmp2;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(*head);
+		return (1);
+	}
+
+	tmp = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	tmp2 = tmp->next;
+	tmp->next = tmp2->next;
+	free(tmp2);
+
+	return (1);
+}
