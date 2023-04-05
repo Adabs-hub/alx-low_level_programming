a37b03582e13edf3664441403edbe5c962222f78
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-04 17:24:59 -0700

    task 9: update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	/*return null if head node is empty*/
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	/*allocate memory for new node*/
