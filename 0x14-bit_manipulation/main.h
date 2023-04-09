894a33e6e4e4e6dedbc900290c778aa61f1ddb9b
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-08 20:42:50 -0700

    task 1: convert to binary

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+	int _putchar(char c);
+	unsigned int _strlen(const char *b);
+	unsigned int binary_to_uint(const char *b);
+#endif /*main.h*/
