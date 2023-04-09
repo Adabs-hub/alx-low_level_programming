7ae6dd3861b031865aefce829af05b14f71bda84
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-09 10:21:49 -0700

    tak 2; get bit by index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,7 @@
 	unsigned int _strlen(const char *b);
 	unsigned int binary_to_uint(const char *b);
 	void print_binary(unsigned long int n);
+	int get_bit(unsigned long int n, unsigned int index);
+
 
 #endif /*main.h*/
