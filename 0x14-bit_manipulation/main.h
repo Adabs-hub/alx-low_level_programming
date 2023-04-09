2173b6316a4324c76e744ee60f5f86f6096bd7e5
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-09 13:13:16 -0700

    task 4: clear bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@
 	void print_binary(unsigned long int n);
 	int get_bit(unsigned long int n, unsigned int index);
 	int set_bit(unsigned long int *n, unsigned int index);
+	int clear_bit(unsigned long int *n, unsigned int index);
 
 
 #endif /*main.h*/
