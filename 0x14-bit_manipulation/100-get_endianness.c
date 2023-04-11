236923a7385df405af5394033312a87b81a76b5f
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-10 18:38:10 -0700

    tast 100: endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	/*first index of uint address is ch*/
+	char *ch = (char *) &i;
+
+	return (*ch);
+}
