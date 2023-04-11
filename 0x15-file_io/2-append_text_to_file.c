f7c5741e5a638f0c5f0c77f858a47366ace73a27
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-10 22:13:08 -0700

    update update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		len = _strlen(text_content);
 		w_count = write(fd, text_content, len);
