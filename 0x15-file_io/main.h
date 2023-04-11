278d2337c340882712a18732a17f6f49b424563d
Adabs-hub <emmanueladabogo@gmail.com>
2023-04-10 19:22:03 -0700

    task 0. copy and print stdout

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+	int _putchar(char c);
+	unsigned int _strlen(const char *b);
+	ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /*main.h*/
