eb959ffd486acc288ce9a77a65a8b06411cce410
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 14:15:37 -0700

    task 0: print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print doubly linked list
+ * @h: head pointer of arg linked list
+ * Return: number of node
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n_node = 0;
+
+	if (!h)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+	}
+}
