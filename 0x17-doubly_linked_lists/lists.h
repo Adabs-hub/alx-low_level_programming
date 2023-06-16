c89ccc43454ae15be62f40da083cb9318f5ab86c
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 18:19:09 -0700

    task 5; 5-main.c test file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,5 +27,6 @@ typedef struct dlistint_s
 	dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 	dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 	void free_dlistint(dlistint_t *head);
+	dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif
