4adc711419b98610e7dd2067db2fbc6cb093bead
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 17:34:27 -0700

    task 4: free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free doubly linked list
+ * @head: head pointer
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = NULL;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head->head->next;
+		free(tmp);
+	}
+}
