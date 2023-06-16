ffb1bee44c1542a7881db05ab5d4586c3fe66843
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 20:18:55 -0700

    task 7; included node insertion at end and beginning of list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,5 +28,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 		*head = new;
-	return (*head);
+	return (new);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+
 /**
  * insert_dnodeint_at_index - add node at giving index of d-list
  * @h: head pointer d-list
@@ -9,6 +10,35 @@
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
+		unsigned int idx, int n)
+{
+	dlistint_t *head = NULL;
+	unsigned int size = 0;
+
+	head = *h;
+	for (size = 0; head != NULL; size++)
+		head = head->next;
+
+	if (size == 1)
+		head = add_dnodeint(h, n);
+	else if (size > 1 && idx < size)
+		head = insert_inbetween_node(h, idx, n);
+	else if (size == idx)
+		head = add_dnodeint_end(h, n);
+	else
+		return (NULL);
+
+	return (head);
+}
+/**
+ * insert_inbetween_node - add node inbetween first and last node
+ * @h: head pointer d-list
+ * @idx: index to add node
+ * @n: value of new node
+ * Return: address to new node
+ */
+
+dlistint_t *insert_inbetween_node(dlistint_t **h,
 		unsigned int idx, int n)
 {
 	dlistint_t *new = NULL, *tmp = NULL, *head = NULL;
