2ca5c6391690e1dd686f64d245e727f2e248a77a
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 18:13:59 -0700

    task 5; get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: head pointer address
+ * @index: index to retrieve
+ * Return: pointer node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (count == index)
+			return (head);
+		head = head->next;
+		count++;
+	}
+
+	return (NULL);
+}
