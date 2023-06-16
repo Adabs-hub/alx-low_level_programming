bcd9d1357f12731bd76f96886511f8c7756506b9
adabs-hub <emmanueladabogo@gmail.com>
2023-06-15 17:20:23 -0700

    task 3: add node at the end of d-list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node to end of d-list
+ * @head: head pointer d-list
+ * @n: value of new node
+ * Return: address to new node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL, *tmp = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n, new->prev = NULL, new->next = NULL;
+	tmp = *head;
+	if (*tmp != NULL)
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next
+		tmp->next = new;
+		new->prev = tmp;
+	}
+	else
+		*head = new;
+	return (*head);
+}
