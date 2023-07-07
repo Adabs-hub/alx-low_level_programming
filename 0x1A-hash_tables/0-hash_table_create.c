bce79ed41f594402366fbd674b0eab1e2b7e0c03
adabs-hub <emmanueladabogo@gmail.com>
2023-07-06 18:49:12 -0700

    task 0:MM

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,17 +10,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table = NULL;
 
-	if (size > 0)
+	h_table = malloc(sizeof(hash_table_t));
+	if (h_table == NULL)
 	{
-		h_table = malloc(sizeof(hash_table_t));
-		if (h_table == NULL)
-		{
-			free(h_table);
-			return (NULL);
-		}
-		h_table->size = size;
-		h_table->array = NULL;
+		free(h_table);
+		return (NULL);
 	}
+	h_table->size = size;
+	h_table->array = malloc(sizeof(char *) * size);
+	if (h_table->array == NULL)
+	{
+		free(h_table->array);
+		free(h_table);
+		return (NULL);
+	}
+
+
 
 	return (h_table);
 }
