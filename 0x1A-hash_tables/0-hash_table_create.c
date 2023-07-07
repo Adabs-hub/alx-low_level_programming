3a4dbc744bba0993e35e7df96e80e7b119b2bce2
adabs-hub <emmanueladabogo@gmail.com>
2023-07-06 18:29:34 -0700

    task 0: create hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates an empty hash table
+ * @size: size of the empty hash table
+ * Return: pointer to new hash tabel
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table = NULL;
+
+	h_table = malloc(sizeof(hash_table_t));
+	if (h_table == NULL)
+	{
+		free(h_table);
+		return (NULL);
+	}
+	h_table->size = size;
+	h_table->array = NULL;
+
+	return (h_table);
+}
