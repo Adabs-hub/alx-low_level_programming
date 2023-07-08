7a99b8ceea9248bb6dd7f0d922bffd2713be25b9
adabs-hub <emmanueladabogo@gmail.com>
2023-07-07 18:18:39 -0700

    task 3: adds element to hash table_M

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table = NULL;
-	int i;
+	unsigned long int i;
 
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
